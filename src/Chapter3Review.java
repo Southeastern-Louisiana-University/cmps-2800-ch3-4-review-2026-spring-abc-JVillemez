19448e86611a73cae674a44ddc8f2ff8ec2d1143
JVillemez <jonah.villemez@selu.edu>
2026-01-27 11:40:58 -0600

    Added the outline from Canvas

--- src/Chapter3Review.java
+++ src/Chapter3Review.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Chapter3Review {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
---
+++ src/Gatekeeper.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+
+public class Gatekeeper {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // --- 1. INPUTS ---
